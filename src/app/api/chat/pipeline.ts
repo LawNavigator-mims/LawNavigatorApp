2711f72f087ecaeb281e825093aeabd569c7bd0b
NaimaAmraan <140434438+NaimaAmraan@users.noreply.github.com>
2025-03-17 00:03:56 -0700

    Load model dynamically

--- src/app/api/chat/pipeline.ts
+++ src/app/api/chat/pipeline.ts
@@ -1,21 +1,20 @@
 import {
   type PipelineType,
   pipeline,
-  env,
   ProgressCallback,
 } from "@huggingface/transformers";
 
 export default class EmbeddingsPipeline {
   static task: PipelineType = "feature-extraction";
   static model = "Xenova/all-MiniLM-L6-v2";
-  static instance: any = null;
+  static instance: any = null; // eslint-disable-line @typescript-eslint/no-explicit-any
 
   static async getInstance(progress_callback?: ProgressCallback) {
     if (this.instance === null) {
-      // NOTE: Uncomment this to change the cache directory
-      // env.cacheDir = './.cache';
-
-      this.instance = pipeline(this.task, this.model, { progress_callback });
+      // Load model dynamically from Hugging Face instead of bundling it
+      this.instance = await pipeline(this.task, this.model, {
+        progress_callback,
+      });
     }
 
     return this.instance;
